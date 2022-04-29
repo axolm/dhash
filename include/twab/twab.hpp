3e20c22d3b1ed2f3daba53ebcaef6325919b192a
axolm <axolm13@gmail.com>
2022-04-29 12:29:27 +0300

    fix

--- include/twab/twab.hpp
+++ include/twab/twab.hpp
@@ -9,36 +9,72 @@ namespace twab::hash {
 
 struct use_pfr {};
 
-namespace __impl {
+// TODO: hasher options.
+// - std containers
+// - ints/string to bytes
+// - endians ?
 
-struct md5_hasher {
-  std::string result() {
-    return "abacaba";
+template<typename HashAlgorightm>
+class hasher {
+public:
+  explicit hasher(HashAlgorightm&& hash_algorithm): hash_algorithm_(std::forward<HashAlgorightm>(hash_algorithm)) {
   }
 
-  void operator()(int x) {
-    std::cerr << "int x: " << x << std::endl;
+  auto result() && {
+    return std::move(hash_algorithm_).result();
   }
 
-  void operator()(std::string s) {
-    std::cerr << "string s: " << s << std::endl;
+  template<typename Data>
+  void operator()(Data&& data) {
+    if constexpr (requires { hash_algorithm_.append(std::forward<Data>(data)); }) {
+      hash_algorithm_.append(std::forward<Data>(data));
+    } else if constexpr (requires { requires std::same_as<typename std::decay_t<Data>::hash, use_pfr>; }) {
+      boost::pfr::for_each_field(std::forward<Data>(data), [this](auto& field) { operator()(field); });
+    } else {  // TODO: else if free func
+      std::forward<Data>(data).hash(*this);
+    }
   }
+
+private:
+  HashAlgorightm hash_algorithm_;
 };
 
 template<typename Hasher, typename Data>
 auto calculate_hash(Hasher hasher, Data&& data) {
-  if constexpr (requires { requires std::same_as<typename std::decay_t<Data>::hash, use_pfr>; }) {
-    boost::pfr::for_each_field(data, [&hasher](auto& field) { hasher(field); });
-  } else {
-    std::forward<Data>(data).hash(hasher);
-  }
+  hasher(std::forward<Data>(data));
   return std::move(hasher).result();
 }
-}  // namespace __impl
+
+class xor_hash_algorithm {
+public:
+  using result_t = uint64_t;
+
+  xor_hash_algorithm() = default;
+
+  void append(uint64_t x) {
+    state_ ^= x;
+  }
+
+  static xor_hash_algorithm from_previous_result(result_t state) {
+    return xor_hash_algorithm(state);
+  }
+
+  result_t result() && {
+    return state_;
+  }
+
+private:
+  explicit xor_hash_algorithm(result_t state): state_(state) {
+  }
+
+  result_t state_{0};
+};
 
 template<typename Data>
-auto md5(Data&& data) {
-  return __impl::calculate_hash(__impl::md5_hasher{}, std::forward<Data>(data));
+auto xor_hash(Data&& data, xor_hash_algorithm algorithm = {}) {
+  hasher hasher(std::move(algorithm));
+  hasher(std::forward<Data>(data));
+  return std::move(hasher).result();
 }
 
 }  // namespace twab::hash
--- tests/test.cpp
+++ tests/test.cpp
@@ -30,17 +30,35 @@ struct MyStruct3 : public MyStruct {
   }
 };
 
+struct OnlyUints {
+  uint64_t x1;
+  uint64_t x2;
+
+  using hash = twab::hash::use_pfr;
+};
+
 int main() {
   MyStruct m1{.x = 1, .s = "afsa"};
   MyStruct2 m2{.x = 2, .s = "abacaba"};
   MyStruct3 m3{MyStruct{.x = 1, .s = "2"}, MyStruct2{.x = 2, .s = "abacaba"}};
 
-  std::cout << twab::hash::md5(m1) << '\n';
-  std::cout << "---\n";
-  std::cout << twab::hash::md5(m2) << '\n';
-  std::cout << "---\n";
-  std::cout << twab::hash::md5(m3) << '\n';
-  std::cout << "---\n";
+
+  OnlyUints uints{.x1 = 228, .x2 = 231};
+  auto xor_hash_result = twab::hash::xor_hash(uints);
+  std::cout << "xor hash #1: " << xor_hash_result << '\n';
+  auto xor_hash_algorithm = twab::hash::xor_hash_algorithm::from_previous_result(xor_hash_result);
+  twab::hash::hasher hasher(std::move(xor_hash_algorithm));
+  OnlyUints more_uints{.x1 = 10, .x2 = 18};
+  hasher(more_uints);
+  std::cout << "xor hash #2: " << std::move(hasher).result() << '\n';
+
+
+  // std::cout << twab::hash::md5(m1) << '\n';
+  // std::cout << "---\n";
+  // std::cout << twab::hash::md5(m2) << '\n';
+  // std::cout << "---\n";
+  // std::cout << twab::hash::md5(m3) << '\n';
+  // std::cout << "---\n";
 
   return 0;
 }
